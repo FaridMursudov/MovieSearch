a9f23190df7b5815a2cf0c88df11d5423fd86b54
Farid Murshudov <f.murshudov@tayqatech.com>
2020-10-16 17:56:49 +0400

    fjg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,6 @@ import { Component } from '@angular/core';
   templateUrl: 'app.component.html',
   styles: ['app.component.scss']
 })
-export class AppComponent {
+export class AppComponent { 
   title = 'MVS';
 }
